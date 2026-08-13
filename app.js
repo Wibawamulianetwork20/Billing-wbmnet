b885b48b9c47fe6890a4b7607f8b9b562a711dcb
Wibawamulianetwork <121048520+Wibawamulianetwork20@users.noreply.github.com>
2026-08-13 12:02:51 +0700

    Update import path for firebase.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { auth } from "./firebase.js";
+import { auth } from "firebase.js";
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
 const menuBtn = document.getElementById("menuBtn");
